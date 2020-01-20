8b2b997bb969f8e274a1d180508a77f1ae53b97f
Luis Everton <luis.everton@outlook.com>
2020-01-20 20:41:27 -0300

    Criada classe Comment

---
+++ ConsoleCsharp_Posts_CursoNelioAlves/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleCsharp_Posts_CursoNelioAlves.Entities
+{
+    class Comment
+    {   //propriedade
+        public string Text { get; set; }
+        //contrutor padrão
+        public Comment()
+        {
+        }
+        //construtor com argumento/parametro
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
