e64de4093a7c66695347cdceeaae95deab94c10a
Luis Everton <luis.everton@outlook.com>
2020-01-20 20:34:06 -0300

    Comit inicial com arquivos iniciais basicos

---
+++ ConsoleCsharp_Posts_CursoNelioAlves/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleCsharp_Posts_CursoNelioAlves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
