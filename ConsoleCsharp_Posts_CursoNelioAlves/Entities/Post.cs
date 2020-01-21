acbe3ae1cbc4cf55df52cebd94494579617dba7b
Luis Everton <luis.everton@outlook.com>
2020-01-21 06:50:47 -0300

    Overrride no ToString da classe Post e execucao do programa com sucesso

--- ConsoleCsharp_Posts_CursoNelioAlves/Entities/Post.cs
+++ ConsoleCsharp_Posts_CursoNelioAlves/Entities/Post.cs
@@ -1,37 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Text;  //necessário para StringBuilder
 
 namespace ConsoleCsharp_Posts_CursoNelioAlves.Entities
 {
     class Post
     {   //propriedades
         public DateTime Moment { get; set; }
-        public string TItle { get; set; }
+        public string Title { get; set; }
         public string Content { get; set; }
         public int Likes { get; set; }
-        public List<Comment> Comments { get; set; } = new List<Comment>;
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
         //construtor padrão
         public Post()
         {
         }
+
         //contrutor com argumento/parametros
-        public Post(DateTime moment, string tItle, string content, int likes)
+        public Post(DateTime moment, string title, string content, int likes)
         {
             Moment = moment;
-            TItle = tItle;
+            Title = title;
             Content = content;
             Likes = likes;
         }
+
         //métodos de adição de comentários
         public void AddComment(Comment comment)
         {
             Comments.Add(comment);
         }
+
         //método de remoção de comentários
         public void Remove(Comment comment)
         {
             Comments.Remove(comment);
+        }
+
+        //sobrecarga da ToString()
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments");
+            foreach (Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
         }
     }
 }
