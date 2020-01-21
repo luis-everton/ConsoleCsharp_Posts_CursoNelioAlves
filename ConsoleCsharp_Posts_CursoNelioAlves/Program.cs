c5cb36596b889889e514a39392c8a30ce35c5553
Luis Everton <luis.everton@outlook.com>
2020-01-21 06:37:09 -0300

    Criada a estrutura de execucao do programa

--- ConsoleCsharp_Posts_CursoNelioAlves/Program.cs
+++ ConsoleCsharp_Posts_CursoNelioAlves/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ConsoleCsharp_Posts_CursoNelioAlves.Entities;
 
 namespace ConsoleCsharp_Posts_CursoNelioAlves
 {
@@ -6,7 +7,20 @@ namespace ConsoleCsharp_Posts_CursoNelioAlves
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that´s awesome");
+            Post p1 = new Post(DateTime.Parse("21/06/2018 13:05:44"), "Traveling to New Zealand", "I going to visit wonderful country!", 12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Goog night");
+            Comment c4 = new Comment("May the force be wthi you");
+            Post p2 = new Post(DateTime.Parse("28/07/2018 23:14:19"), "Good night guys", "See you tomorrou", 5);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
         }
     }
 }
